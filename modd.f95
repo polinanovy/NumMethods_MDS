66c850fcb279b9db2d71e32923764b339db7c3bf
Polina <103999174+polinanovy@users.noreply.github.com>
2023-11-28 19:21:42 +0300

    Update modd.f95

--- modd.f95
+++ modd.f95
@@ -8,7 +8,7 @@ contains
 subroutine InitializeParameters(L_x, L_y, N, M, sigma, T_x, dT_y, T_0, D)
 ! Subroutine for setting model parameters
 integer :: N, M
-real(8) :: L_x, L_y, sigma, T_x, dT_y, T_0, D
+real(8) :: L_x, L_y, sigma, T_x, dT_y, T_0, D, C
 open(unit = 1, file = 'INPUT')
 read(1, *) L_x
 read(1, *) L_y
@@ -20,5 +20,23 @@ read(1, *) dT_y
 read(1, *) T_0
 read(1, *) D
 end subroutine InitializeParameters
+
+subroutine InitializeGrid(N, M, D, L_x, L_y, x, dx, y, dy, dt)
+! Subroutine for initialising the grid
+real(8) :: L_x, L_y, D
+integer :: N, M, i
+real(8) :: dx, dt, x(0:N-1), dy, y(0:M-1)
+dx = L_x / (N - 1)
+dy = L_y / (M - 1)
+dt = 
+x(0) = 0; x(N-1) = L_x
+do i = 1, N-2
+	x(i) = x(i-1) + dx
+enddo
+y(0) = 0; y(N-1) = L_y
+do i = 1, M-2
+	y(i) = y(i-1) + dy
+enddo
+end subroutine InitializeGrid
 
 end module
