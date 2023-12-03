f64d40867cc2a3c6d30bdd6f1a38f1c3ddd7b3b2
distant-earth <96383985+distant-earth@users.noreply.github.com>
2023-12-03 18:52:10 +0300

    Plotting tool added

---
+++ plotting.py
@@ -0,0 +1,38 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+import matplotlib.cm
+from matplotlib import colors
+import matplotlib
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams.update({'font.size': 18})
+matplotlib.rcParams['mathtext.fontset'] = 'custom'
+matplotlib.rcParams['mathtext.rm'] = 'Times New Roman'
+matplotlib.rcParams['mathtext.it'] = 'Times New Roman:italic'
+matplotlib.rcParams['mathtext.bf'] = 'Times New Roman:bold'
+
+with open('RESULT') as table:
+	line1 = table.readline()
+	N = int(line1.split()[0])
+	M= int(line1.split()[1])
+	dt = float(line1.split()[2])
+	t_stop = float(line1.split()[3])
+	u = np.empty([N, M])
+	for index, line in enumerate(table):
+		u[index,:] = line.split()
+
+mmax = u.max()
+mmin = u.min()
+plt.figure(figsize=(8,6))
+#plt.title(r'$\Delta P$'+'\n'+fr'$(e = {round(ecc0, 2)},~d = {round(d, 2)}$'+r'$R_E$, '+fr'$A$/$C = {round(AC, 2)}$, $B$/$C = {round(BC, 2)})$', fontsize = 18)
+#plt.xlabel(r'$P_0$, ч')
+#plt.ylabel(r'$\varphi_0,$ град')
+#plt.gca().xaxis.set_major_locator(MultipleLocator(10))
+#plt.gca().xaxis.set_major_formatter('{x:.0f}')
+cax = plt.imshow(u, interpolation = 'none', origin = 'lower', aspect = 'auto', extent = (0,0.5,0,0.5), cmap = 'Reds')
+#myticks = [int(round(mmin, -1) + 10 * i) for i in range(int((mmax - mmin) // 10 + 1))]
+plt.colorbar(cax)#, ticks=myticks).ax.set_yticklabels(myticks)
+plt.savefig('map.png', format = 'png', dpi=600)
+
+
+plt.show()
